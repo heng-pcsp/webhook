21985676a6122ef6c7671c44502f00f6649d3a82
heng-pcsp <heng@pcspgroup.com>
2018-01-30 13:44:46 +0700

    bot

--- bot.php
+++ bot.php
@@ -153,27 +153,29 @@ function processMessage($message) {
 }
 
 
-if (php_sapi_name() == 'cli') {
-  // if run from console, set or delete webhook
-  apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
-  exit;
-}
+// if (php_sapi_name() == 'cli') {
+//   // if run from console, set or delete webhook
+//   apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
+//   exit;
+// }
 
 
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
+// $content = file_get_contents("php://input");
+// $update = json_decode($content, true);
 
-if (!$update) {
-  // receive wrong update, must not happen
-  // exit;
-}
+// if (!$update) {
+//   // receive wrong update, must not happen
+//   // exit;
+// }
 
-if (isset($update["message"])) {
-  // processMessage($update["message"]);
+// if (isset($update["message"])) {
+//   // processMessage($update["message"]);
 
-  apiRequestJson("sendMessage", array('chat_id' => 549381667, "text" => 'New Message'));
-}
-else
-{
-  apiRequestJson("sendMessage", array('chat_id' => 549381667, "text" => "Haven't message"));
-}
+//   apiRequestJson("sendMessage", array('chat_id' => 549381667, "text" => 'New Message'));
+// }
+// else
+// {
+//   apiRequestJson("sendMessage", array('chat_id' => 549381667, "text" => "Haven't message"));
+// }
+
+apiRequestJson("sendMessage", array('chat_id' => 549381667, "text" => 'Hello World!'));
